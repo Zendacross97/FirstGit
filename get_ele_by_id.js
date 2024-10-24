6261a6c8ece3adc8f7ba2018b0d60420ac326ca8
Zendacross97 <sidhchakraboty66@gmail.com>
2024-10-24 21:41:11 +0530

    get element by Id

---
+++ get_ele_by_id.js
@@ -0,0 +1,10 @@
+let heading=document.getElementById(`main-heading`);
+heading.textContent=`Fruit World`;
+heading.style.color=`orange`;
+let head=document.getElementById(`header`);
+head.style.backgroundColor=`green`;
+head.style.borderBottom=`3px solid orange`;
+let basket=document.getElementById(`basket-heading`);
+basket.style.color=`green`;
+let thanks=document.getElementById(`thanks`);
+thanks.innerHTML=`<p>Please visit us again</p>`;
