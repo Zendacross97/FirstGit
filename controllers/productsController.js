844197f225579b899eeaaff49c68e08c6c95ca4f
Zendacross97 <sidhchakraboty66@gmail.com>
2025-04-05 21:25:00 +0530

    GET request with HTML-2

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,3 +1,22 @@
+// Deliverables:
+
+// Your mission is to build a Node.js application that serves an HTML file in response to a GET request.
+
+// Set up a server using express.
+// Add a GET endpoint (e.g., /api/products) to handle GET requests.
+// Use res.sendFile() to serve an HTML file from your project directory inside folder VIEW.
+// Return a form with following attributes:
+// Form Structure:
+
+// The form should contain a label with the text "Product Name".
+// The label's for attribute and the input's id attribute should both be set to product for consistency and accessibility.
+// The form should include an input field where users can enter the product name. The input field should have the name attribute set to productName.
+// The form should contain a submit button with the content "Add Product".
+
+// Keep your project organized and fun to explore!
+
+// Also try to run the given setup on VS Code on your system and once approved push your code to GitHub.
+
 const path = require('path');
 
 const getAllProducts = (req, res) => {//send file = to redirect when api is called through an end point
